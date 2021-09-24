2df5f25341b64332d4a9919914b86fe9bf5b5a85
develohjh <oojh95@naver.com>
2021-09-24 22:49:22 +0900

    0924TIL

---
+++ BAEKJOON/BAEK2116.py
@@ -0,0 +1,13 @@
+# 주사위 탑 쌓기
+def work(dice_num, top):
+    if dice[dice_num][top]
+
+    work(dice[])
+
+
+N = int(input())
+
+dice = [list(map(int, input().split())) for _ in range(N)]
+(0,5) (1,3) (2,4)
+for i in range(6):
+    work(0, i)
