4c192449a8f2a26ee12ba43790bbdefef66fc4bc
unknown <oojh95@naver.com>
2021-11-06 16:16:34 +0900

    1106_1

---
+++ SWEA/D3/SWEA11387.py
@@ -0,0 +1,9 @@
+# 몬스터 사냥
+T = int(input())
+for tc in range(1, T+1):
+    D, L, N = map(int, input().split())
+    result = 0
+    for i in range(N):
+        result += D*(1+i*L/100)
+
+    print('#{} {}'.format(tc, int(result)))
