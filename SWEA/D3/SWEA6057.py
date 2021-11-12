71e81fda66fb127178343dd3251f35b00c534bbf
unknown <oojh95@naver.com>
2021-11-12 18:36:42 +0900

    1112_1

---
+++ SWEA/D3/SWEA6057.py
@@ -0,0 +1,17 @@
+# 그래프의 삼각형
+T = int(input())
+for tc in range(1, T+1):
+    N, M = map(int, input().split())
+    arr = [[0] * (N+1) for _ in range(N+1)]
+    for _ in range(M):
+        x, y = map(int, input().split())
+        arr[x][y] = 1
+        arr[y][x] = 1
+    cnt = 0
+    for i in range(1, N+1):
+        for j in range(i+1, N+1):
+            if arr[i][j] == 1:
+                for k in range(i+1, N+1):
+                    if arr[j][k] == 1 and arr[i][k] == 1:
+                        cnt += 1
+    print('#{} {}'.format(tc, cnt))
