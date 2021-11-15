a0514e47e6642ac6f59d21e57f51c9d91bdf3f5c
unknown <oojh95@naver.com>
2021-11-15 19:07:28 +0900

    1115_2

---
+++ SWEA/D3/SWEA5789.py
@@ -0,0 +1,10 @@
+#상자 바꾸기
+T = int(input())
+for tc in range(1, T+1):
+    N, Q = map(int, input().split())
+    arr = [0] * (N+1)
+    for i in range(1, Q+1):
+        L, R = map(int, input().split())
+        for j in range(L, R+1):
+            arr[j] = i
+    print('#{}'.format(tc), *arr[1:])
